2720bb05508c39fe4ac691d6d7ed7d619782fba4
Dave Herrera <dav3life@gmail.com>
2021-08-10 15:06:41 -0500

    Update bep20sol.sol

--- bep20sol.sol
+++ bep20sol.sol
@@ -51,3 +51,33 @@ return true;
 }
 /**
 @dev Sets `amount` as the allocation of `spender` over the Caller's tokens.
+
+
+
+function approve(address _spender, uint256 _value)
+public
+returns (bool success)
+{
+allowance[msg.sender][_spender] = _value;
+emit Approval(msg.sender, _spender, _value);
+return true;
+}
+/**
+* @dev Moves the `amount` tokens from `sender` to `recipient` using the allowance mechanism. `amount` is then deducted from the Caller's allowance.
+
+
+function transferFrom(
+address _from,
+address _to,
+uint256 _value
+public returns (bool success) {
+require(_value ;= balanceOf[_from]);
+require(_value ;= allowance[_from][msg.sender]);
+balanceOf[_from] -= _value;
+balanceOf[_to] += _value;
+allowance[_from][msg.sender] -= _value;
+emit Transfer(_from, _to, _value);
+return true;
+}
+}
+
